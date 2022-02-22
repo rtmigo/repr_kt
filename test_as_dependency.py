4a923b5c2eff8d931e09292fa2bed64c69be2a71
Artem IG <ortemeo@gmail.com>
2022-02-23 01:28:28 +0300

    kitest

--- test_as_dependency.py
+++ test_as_dependency.py
@@ -2,15 +2,15 @@
 
 from kitest import run_with_git_dependency
 
-result = run_with_git_dependency(
+with AppWithGitDependency(
     module="io.github.rtmigo:repr",
     url="https://github.com/rtmigo/repr_kt",
+    branch="dev",
     main_kt="""
         import io.github.rtmigo.repr.*
         fun main() = println(listOf(1, 2, 3).toRepr())
-    """)
+    """) as app:
 
-if result.text.strip() != "listOf(1, 2, 3)":
-    exit(1)
+    app.run().assert_output_is("listOf(1, 2, 3)")
 
 print("Everything is OK!")
